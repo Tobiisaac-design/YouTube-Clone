ddb42adf0a10a8cf24818ea818ff402003518ccb
Tobi Isaac <tobiisaac428@gmail.com>
2024-06-13 06:10:07 +0100

    new folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Video from "./pages/Video/Video";
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Navbar.css";
-import menu_icon from "../assets/menu.png";
-import logo from "../assets/logo.png";
-import Search_icon from "../assets/search.png";
-import upload_icon from "../assets/upload.png";
-import more_icon from "../assets/more.png";
-import notification_icon from "../assets/notification.png";
-import profile_icon from "../assets/boy.png";
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import Search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/boy.png";
 
 const Navbar = () => {
   return (
